1f9ef003fe3da9e866138d33eec93beca519378f
Adonis <adonism10101@gmail.com>
2017-07-29 07:02:07 +0000

    yay

---
+++ govhack.py
@@ -0,0 +1,82 @@
+import csv,math
+
+class Thing:
+  def __init__(self, i, d, l, t, c1, c2):
+    self.id=i.upper()
+    self.district=d.upper()
+    self.location=l.upper()
+    self.type=t.upper()
+    self.lat=c1
+    self.long=c2
+  
+  def __repr__(self):
+    return "There's a {} {} in {}, {}, ({},{})".format(self.type, ("BBQ" if self.id[0]=="B" else ""), self.location, self.district, self.lat, self.long)
+
+f=list(csv.reader(open("Drinking_Fountains.csv")))
+b=list(csv.reader(open("Public_Barbeques_in_the_ACT.csv")))
+t=list(csv.reader(open("Public_Toilets_in_the_ACT.csv")))
+
+asc=open("art.txt").read().split("\n\n")
+
+f=[Thing(x[0],x[1],x[2],"WATER FOUNTAIN",float(x[5]),float(x[6])) for x in f[1:]]
+b=[Thing(x[0],x[1],x[2],x[3],float(x[5]),float(x[6])) for x in b[1:]]
+t=[Thing(x[0],x[1],x[2],x[3],float(x[5]),float(x[6])) for x in t[1:]]
+
+burbs=[x[4] for x in csv.reader(open("ACT_Division_Boundaries_data.csv"))][1:]
+locs={x.location for x in f+b+t}-{""}
+dists={x.district for x in f+b+t}
+
+d={"TOILET":t,"LOO":t,"WATER":f,"DRINK":f,"FOUNTAIN":f,"WATER FOUNTAIN":f,"BBQ":b,"BARBEQUE":b}
+
+#----------------------------------------------------------------
+
+def near(l,w):
+  return [x for x in d[w] if l in x.district]
+
+def near5(c,w):
+  c=[float(x) for x in c.strip("()").split(",")]
+  return sorted(d[w],key=lambda x:dist(c[0],c[1],x.lat,x.long))[:5]
+
+def iscoord(s):
+  try:
+    x,y=map(float,s.strip("()").split(","))
+  except:
+    return False
+  return -90<=x<=90 and -180<=y<=180
+
+def dist(x1,y1,x2,y2):
+  dl=math.radians(y2-y1)
+  df=math.radians(x2-x1)
+  fm=math.radians(x1+x2)/2
+  return 6371000*math.hypot(dl*math.cos(fm),df)
+
+print("\n"*5)
+print(asc[0])
+print("\n"*3)
+l=input(asc[1]+"\n").upper()
+while l not in locs|dists and not iscoord(l):
+  print("Huh never heard of it. Try a REAL suburb or some proper coords.")
+  l=input("So, where you at fam? ").upper()
+w=input("\n"*2+asc[2]+"\n").upper()
+
+if "MEME" in w:
+  input("I warned you")
+  input("This is my only one")
+  print(asc[3])
+  w=input("Aight so whachu really after? ").upper()
+while w not in d:
+  print("Da fuks dat? We only do bbq, toilet n water.")
+  w=input("Aight so whachu really after? ").upper()
+
+if iscoord(l):
+  n=near5(l,w)
+else:
+  n=near(l,w)
+  if 0<len(n)<5:
+    n=near5(str((n[0].lat,n[0].long)),w)
+ 
+if n:
+  print("Aight found some")
+  for i in n: print(i)
+else:
+  print("Fugg there's no "+w.lower()+" near you. Bad luck bro.")
